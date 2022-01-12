07670f37bcb4a5772c9aa9c210d0820e182e7f94
AndrePedro51 <andrepedro51@gmail.com>
2022-01-12 14:13:46 -0300

    Exemplos de SSR

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,33 @@
-import { Box, Button, Stack } from "@chakra-ui/react";
+import { Box, Button, Stack, Text } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
-export function Pagination(){
+interface PaginationProps {
+    totalCountOfRegisters: number;
+    registersPerPage?: number;
+    currentPage?: number;
+    onPageChange:(page: number) => void;
+}
+
+const sibilingCount = 1
+
+function generatePageArray(from: number, to: number){
+    return [...new Array(to - from)]
+        .map((_ , index) => { return from + index + 1})
+        .filter(page => page > 0)
+}
+
+export function Pagination({ 
+    totalCountOfRegisters, 
+    registersPerPage = 10, 
+    currentPage = 1, 
+    onPageChange 
+}: PaginationProps){
+    const lastPage = Math.floor(totalCountOfRegisters / registersPerPage)
+
+    const previousPages = currentPage > 1 ? generatePageArray(currentPage - 1 - sibilingCount, currentPage - 1) : []
+
+    const nextPages = currentPage < lastPage ? generatePageArray(currentPage, Math.min(currentPage + sibilingCount, lastPage)): []
+    
     return(
         <Stack
             direction={["column","row"]}
@@ -14,9 +40,31 @@ export function Pagination(){
                 <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
             </Box>
             <Stack direction="row" spacing="2">
-                <PaginationItem isCurrent number={1}/>
-                <PaginationItem number={2}/>
-                <PaginationItem number={3}/>
+                {currentPage > (1 + sibilingCount) && (
+                    <>
+                        <PaginationItem onPageChange={onPageChange} number={1}/>
+                        { currentPage > (2 + sibilingCount) && <Text color="grat.300" w="8" textAlign="center">...</Text>}
+                    </>
+                    
+                )}
+
+                {previousPages.length > 0 && previousPages.map(page => {
+                    return <PaginationItem onPageChange={onPageChange}  key={page} number={page}/>
+                })}
+
+                <PaginationItem onPageChange={onPageChange}  isCurrent number={currentPage}/>
+
+                {nextPages.length > 0 && nextPages.map(page => {
+                    return <PaginationItem onPageChange={onPageChange}  key={page} number={page}/>
+                })}
+
+                {(currentPage + sibilingCount) < lastPage && (
+                    <>
+                        { (currentPage + 1 + sibilingCount) < lastPage && <Text color="grat.300" w="8" textAlign="center">...</Text>}
+                        <PaginationItem onPageChange={onPageChange}  number={lastPage} />
+                    </>
+                    
+                )}
             </Stack>
             
         </Stack>
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -7,6 +7,10 @@ import { Sidebar } from "../../components/Sidebar";
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserDataForm = {
     name: string;
@@ -23,6 +27,22 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser(){
+    const router = useRouter()
+    const createUser = useMutation(async (user: CreateUserDataForm) => {
+        const response = await api.post('users', {
+            user:{
+                ...user,
+                created_at: new Date()
+            }
+        })
+
+        return response.data.user
+    },{
+        onSuccess: () => {
+            queryClient.invalidateQueries('users')
+        }
+    })
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
     })
@@ -30,9 +50,9 @@ export default function CreateUser(){
     const { errors } = formState
 
     const handleCreateUser: SubmitHandler<CreateUserDataForm> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000))
+        await createUser.mutateAsync(values)
 
-        console.log(values)
+        router.push('/users')
     }
     return(
         <Box>
